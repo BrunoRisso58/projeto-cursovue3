e1b1ef3d6ae2bb99910058db187bdb62d179a8c4
Bruno Risso <brunorisso58@gmail.com>
2022-09-15 14:48:43 -0300

    Criando banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
+  <Banner />
   <div class="main-container">
     <h1>HOME</h1>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue';
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    Banner
+  }
 }
 </script>
