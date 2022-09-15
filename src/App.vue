c56778237e42e3f5cd4f70eb335759049c5dceea
Bruno Risso <brunorisso58@gmail.com>
2022-09-15 14:22:07 -0300

    Estilizando navbar e footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavBar />
+  <NavBar :logo="src_logo" :alt="alt_logo" />
   <router-view/>
   <Footer />
 </template>
@@ -12,9 +12,33 @@ export default {
   components: {
     NavBar,
     Footer
+  },
+  data() {
+    return {
+      src_logo: "/img/logo.png",
+      alt_logo: "logo"
+    }
   }
 }
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222
+  }
 </style>
