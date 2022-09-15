730925c92c72c9fb173e5e910083eba9ae345fbc
Bruno Risso <brunorisso58@gmail.com>
2022-09-15 13:44:06 -0300

    Configurações Iniciais

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" alt="logo">
+        </router-link>
+        <router-link to="/">HOME</router-link> |
+        <router-link to="/pedidos">PEDIDOS</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
